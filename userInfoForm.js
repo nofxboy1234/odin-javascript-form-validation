188ccc17a100a6892326b7b5584e2814e335edac
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-08 18:33:50 +0200

    Add javascript to check validity

--- userInfoForm.js
+++ userInfoForm.js
@@ -1,24 +1,31 @@
-// const form = document.querySelector("form");
-// const email = document.getElementById("mail");
-// const emailError = document.querySelector(".error");
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const emailError = document.querySelector(".error");
 
-// email.addEventListener("input", (event) => {
-//   if (email.validity.valid) {
-//     emailError.textContent = "";
-//     emailError.className = "error";
-//   } else {
-//     showError();
-//   }
-// });
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "error";
+  } else {
+    showError();
+  }
+});
 
-// function showError() {
-//   if (email.validity.valueMissing) {
-//     emailError.textContent = "Email address is required";
-//   } else if (email.validity.typeMismatch) {
-//     emailError.textContent = "Value needs to be a valid email address";
-//   } else if (email.validity.tooShort) {
-//     emailError.textContent = `Email should be at least ${email.minLength} characters. You entered ${email.value.length}.`;
-//   }
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
 
-//   emailError.className = "error active";
-// }
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "Email address is required";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Value needs to be a valid email address";
+  } else if (email.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${email.minLength} characters. You entered ${email.value.length}.`;
+  }
+
+  emailError.className = "error active";
+}
