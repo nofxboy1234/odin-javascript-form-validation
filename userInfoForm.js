0f4247586df65e44dc3bb03cf1c86b3544c6e813
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-08 18:17:56 +0200

    Let email be :invalid if it is empty
    Even with form novalidate, the input:invalid css pseudo class is matched.
    
    The Boolean required attribute, if present, indicates that the user must
    specify a value for the input BEFORE the owning form can be submitted.

---
+++ userInfoForm.js
@@ -0,0 +1,24 @@
+// const form = document.querySelector("form");
+// const email = document.getElementById("mail");
+// const emailError = document.querySelector(".error");
+
+// email.addEventListener("input", (event) => {
+//   if (email.validity.valid) {
+//     emailError.textContent = "";
+//     emailError.className = "error";
+//   } else {
+//     showError();
+//   }
+// });
+
+// function showError() {
+//   if (email.validity.valueMissing) {
+//     emailError.textContent = "Email address is required";
+//   } else if (email.validity.typeMismatch) {
+//     emailError.textContent = "Value needs to be a valid email address";
+//   } else if (email.validity.tooShort) {
+//     emailError.textContent = `Email should be at least ${email.minLength} characters. You entered ${email.value.length}.`;
+//   }
+
+//   emailError.className = "error active";
+// }
